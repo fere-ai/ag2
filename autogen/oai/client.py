fe4f5a75136c6bc47e8cc314278633ca9665d1f5
Pranav Prakash <pranav@bitpack.me>
2025-06-02 13:34:09 +0530

    Updating to use StreamMessage

--- autogen/oai/client.py
+++ autogen/oai/client.py
@@ -579,8 +579,7 @@ class OpenAIClient:
 
                         # If content is present, print it to the terminal and update response variables
                         if content is not None:
-                            iostream.print(process_llm_token(content), end="", flush=True)
-                            iostream.send(StreamMessage(content=content))
+                            iostream.send(StreamMessage(content=process_llm_token(content)))
                             response_contents[choice.index] += content
                             completion_tokens += 1
                         else:
